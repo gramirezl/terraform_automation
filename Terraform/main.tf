d81828bb374f26dfed4d727a471375a41a97927d
Guztrap <gusiz@hotmail>
2025-03-05 14:51:44 -0600

    se agregan los providers

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,9 +1,17 @@
-# Configure the Azure provider
+# Configure the Azure, AWS, and Google providers
 terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = ">= 2.26"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.0"
+    }
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 3.5"
     }
   }
 
@@ -12,4 +20,13 @@ terraform {
 
 provider "azurerm" {
   features {}
+}
+
+provider "aws" {
+  region = "us-west-2"  # Cambia la región según tus necesidades
+}
+
+provider "google" {
+  project = "your-gcp-project-id"  # Cambia el ID del proyecto según tus necesidades
+  region  = "us-central1"          # Cambia la región según tus necesidades
 }
